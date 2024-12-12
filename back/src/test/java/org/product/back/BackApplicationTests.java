3fe6f62913665f2598d8969010c702f59f995fc4
xersky <ayoub.elaoufi@gmail.com>
2024-12-12 16:44:12 +0100

    creating a starter Spring Boot app

---
+++ back/src/test/java/org/product/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.product.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
