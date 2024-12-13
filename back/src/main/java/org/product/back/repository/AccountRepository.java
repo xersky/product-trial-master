bc3007f42fa4ac3e7b3f062c6ba0545f5b56f0e7
xersky <ayoub.elaoufi@gmail.com>
2024-12-13 04:08:20 +0100

    adding the Account Repository

--- back/src/main/java/org/product/back/repository/AccountRepository.java
+++ back/src/main/java/org/product/back/repository/AccountRepository.java
@@ -4,7 +4,9 @@ import org.product.back.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    Account findByEmail(String email);
+    Optional<Account> findByEmail(String email);
 }
