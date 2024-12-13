4cea3696c44efcaa7622ab07cc38e2448e410980
xersky <ayoub.elaoufi@gmail.com>
2024-12-13 04:03:16 +0100

    adding UserDTO for abstraction

---
+++ back/src/main/java/org/product/back/dto/UserDTO.java
@@ -0,0 +1,11 @@
+package org.product.back.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+    private String email;
+    private String password;
+}
